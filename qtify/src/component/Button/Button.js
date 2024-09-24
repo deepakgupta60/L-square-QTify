7c6b3fb5645816029768b4418739b64f4f8854af
Deepak Gupta <deepakgupta60k@gmail.com>
2024-09-24 17:37:25 +0530

    working

--- qtify/src/component/Button/Button.js
+++ qtify/src/component/Button/Button.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+
 import { Button } from '@mui/material'
+import styles from "./Button.module.css";
 
 const BasicButton = ({children}) => {
   return (
-    <Button sx={{backgroundColor:"#121212", color:"#34C94B", fontFamily:"poppins"}}>
+    <Button style={{backgroundColor:"#121212", color:"#34C94B"}} className={styles.feedBackBtn}>
         {children}
     </Button>
   )
