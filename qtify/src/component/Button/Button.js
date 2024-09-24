d503254136020f578fa05f52d81fe6744f99e848
Deepak Gupta <deepakgupta60k@gmail.com>
2024-09-24 18:22:31 +0530

    working on navbar

--- qtify/src/component/Button/Button.js
+++ qtify/src/component/Button/Button.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 import { Button } from '@mui/material'
-import styles from "./Button.module.css";
+// import styles from "./Button.module.css";
 
 const BasicButton = ({children}) => {
   return (
-    <Button style={{backgroundColor:"#121212", color:"#34C94B"}} className={styles.feedBackBtn}>
+    <Button style={{backgroundColor:"#121212", color:"#34C94B", fontFamily: "Poppins, sans-serif"}} >
         {children}
     </Button>
   )
